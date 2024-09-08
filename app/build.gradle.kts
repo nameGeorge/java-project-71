071cd260db8b8718c41f1f998f069824d5629f50
nameGeorge <geomel@yandex.ru>
2024-09-08 20:47:42 +0300

    Step 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,36 +1,40 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
-    id("java")
-    id ("application")
-    id ("checkstyle")
-    id ("jacoco")
+    application
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "hexlet.code"
+group = "io.hexlet"
+
 version = "1.0-SNAPSHOT"
 
-application {
-    mainClass = "hexlet.code.App"
-}
-repositories {
-    mavenCentral()
-}
+application { mainClass.set("io.hexlet.Application") }
 
-
+repositories { mavenCentral() }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("info.picocli:picocli:4.7.6")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.1")
-    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
-    implementation ("org.apache.commons:commons-collections4:4.5.0-M2")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
 
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(true) }
-}
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
